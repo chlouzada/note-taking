d12149895e2a00b9f0dfc7963fd00f3d57e1bca2
carlos <chlouzada@gmail.com>
2022-09-20 20:27:27 -0300

    refactor editor text sync

--- apps/nextjs/src/components/Editor/index.tsx
+++ apps/nextjs/src/components/Editor/index.tsx
@@ -1,51 +1,43 @@
 import { useEffect, useRef, useState } from "react";
 import { trpc } from "../../utils/trpc";
+import { useSelection } from "../../layouts/AppLayout";
 import MarkdownIt from "markdown-it";
 import MdEditor from "react-markdown-editor-lite";
 
 import "react-markdown-editor-lite/lib/index.css";
 import "./styles.module.css";
-import { useSelection } from "../../layouts/AppLayout";
 
-const mdParser = new MarkdownIt(/* Markdown-it options */);
+const useDebounce = (value: any, delay: number) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+  useEffect(() => {
+    if (value === undefined) return;
+    const handler = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+    return () => clearTimeout(handler);
+  }, [value, delay]);
+  return debouncedValue;
+};
+
+const parser = new MarkdownIt(/* Markdown-it options */);
 
 export function Editor() {
+  const [text, setText] = useState<string>();
   const [height, setHeight] = useState(0);
   const { noteId } = useSelection();
   const ref = useRef<HTMLDivElement>(null);
-  let timeoutId: NodeJS.Timeout;
+  const debounced = useDebounce(text, 500);
 
   const { data: note } = trpc.note.get.useQuery(noteId!, { enabled: !!noteId });
-
   const update = trpc.note.update.useMutation();
-
-  const handleEditorChange = ({
-    html,
-    text,
-  }: {
-    html: string;
-    text: string;
-  }) => {
-    if (!note) return;
-
-    clearTimeout(timeoutId);
-
-    timeoutId = setTimeout(() => {
-      const data = update.mutate({
-        id: note.id,
-        data: {
-          content: text,
-        },
-      });
-
-      console.log(data);
-      clearTimeout(timeoutId);
-    }, 1000);
-  };
 
   useEffect(() => {
     setHeight((ref.current as any)?.clientHeight);
   }, []);
+
+  useEffect(() => {
+    update.mutate({ id: noteId!, data: { content: debounced } });
+  }, [debounced]);
 
   return (
     <div ref={ref} className="col-start-3 col-end-8">
@@ -53,8 +45,8 @@ export function Editor() {
         <MdEditor
           syncScrollMode={["leftFollowRight", "rightFollowLeft"]}
           style={{ height }}
-          renderHTML={(text: string) => mdParser.render(text)}
-          onChange={handleEditorChange}
+          renderHTML={(text: string) => parser.render(text)}
+          onChange={({ text }) => setText(text)}
           defaultValue={note.content}
         />
       )}
