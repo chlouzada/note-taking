64ec0c458485a912b13f1580d4adb88ec7d2d998
carlos <chlouzada@gmail.com>
2022-09-22 18:30:18 -0300

    add secret to schema

--- apps/nextjs/src/env/schema.mjs
+++ apps/nextjs/src/env/schema.mjs
@@ -8,6 +8,7 @@ import { z } from "zod";
 export const serverSchema = z.object({
   NODE_ENV: z.enum(["development", "test", "production"]),
   DATABASE_URL: z.string(),
+  JWT_SECRET: z.string(),
 });
 
 /**
